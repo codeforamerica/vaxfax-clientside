a050834156221eb9b54f84cc35c7839304dd3e6e
Kevin B <kberry@codeforamerica.org>
2016-02-02 23:40:02 -0800

    added javascript to submit and ensured error handling is handled

--- vaxfax.js
+++ vaxfax.js
@@ -1,40 +1,92 @@
 
+function successText(faxId) {
+    return "Success! We have received your fax and your \
+    fax id is " + faxId + ". We will update you \
+    when the records have been sent to the school."
+}
 
-function sendDataToServer(formData) {
-	return $.ajax({
-  		type: "POST",
-		url: "http://app-2207.on-aptible.com/fax",
-  		data: formData,
-  		success: function(returnData){
-  			window.location.href = "success.html"
-  			console.log(returnData)
-  		},
-  		error: function(errorData){
-  			console.log("error")
-  			console.log(errorData)
-  		},
-  		dataType: "json",
-  		contentType : "application/json"
-	});
+function failureText(faxId) {
+    return "We are sorry, but your request was unsuccessful. \
+    Please try again. If you continue to have issues, please \
+    call the Kansas City Department of Health Services at \
+    ***-***-****."
+}
+
+function processResult(result) {
+    if(result === true) {
+        $("#onlineForm")[0].reset();
+        window.location.href = "success.html"
+    }
+}
+
+function createDialog(dialogCopy, clear) {
+    return $("#dialog").dialog({
+        create: function( event, ui ) {
+            $("#dialogContent").html(dialogCopy)
+        },
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "ok": function() {
+                $(this).dialog("destroy");
+                processResult(clear);
+            }
+        }
+    });
+}
+
+function successCallbackFunction(dataResponse) {
+    var returnText = successText(dataResponse['fax_id'])
+    createDialog(returnText, true).dialog("open")
+}
+
+function failureCallbackFunction(dataResponse) {
+    var returnText = failureText()
+    createDialog(returnText, false).dialog("open")
+}
+
+function sendDataToServer(formData, successFunction, errorFunction) {
+    return $.ajax({
+        type: "POST",
+        url: "http://app-2207.on-aptible.com/fax",
+        // url: "http://127.0.0.1:5000/fax",
+        data: formData,
+        success: function(returnData){
+            successFunction(returnData)
+        },
+        error: function(errorData){
+            errorFunction(errorData)
+        },
+        contentType : "application/json"
+    });
 }
 
 
 function submitItem(submitId, formId) {
-	return $(submitId).submit(function( event ) {
-		var formData = JSON.stringify($(formId).serializeArray());
-	  	event.preventDefault();
-	  	sendDataToServer(form)
-	});	
+    return $(submitId).submit(function( event ) {
+        var formData = JSON.stringify($(formId).serializeArray());
+        event.preventDefault();
+        sendDataToServer(formData);
+    }); 
+}
+
+function getCalendarDate() {
+    var year  = $("#dob-year").val();
+    var month = $("#dob-month").val();
+    var day   = $("#dob-day").val();
+    return month + "-" + day + "-" + year;
 }
 
 $(function() {
- //    submitItem("#onlineSubmit", "#onlineForm")
- 	console.log(location.pathname)
- 	$("#onlineForm").submit(function( event ) {
-		event.preventDefault();
-		var formData = JSON.stringify($("#onlineForm").serializeArray());
-		sendDataToServer(formData)
-	});	
+    $("#onlineForm").submit(function( event ) {
+        event.preventDefault();
+        var formData = $("#onlineForm").find(':input').serializeArray()
+            .reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
+        formData['child_dob'] = getCalendarDate()
+        formData = JSON.stringify(formData)
+    
+        sendDataToServer(formData, successCallbackFunction, failureCallbackFunction)
+    }); 
 
 
 });
